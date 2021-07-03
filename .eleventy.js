2d68ac761118c7defcdef4505e7606b298cdd933
singhkunal2050 <singhkunal2050@gmail.com>
2021-07-03 10:42:54 +0530

    sass setup

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,13 @@
+// const pluginSass = require("eleventy-plugin-sass");
+
+
 module.exports = function(eleventyConfig) {
   // Add a filter using the Config API
   eleventyConfig.addFilter( "myFilter", function() {});
+
+  eleventyConfig.addWatchTarget("./src/sass/");
+
+  eleventyConfig.addPassthroughCopy("./src/css");
 
   // You can return your Config object (optional).
   return {
