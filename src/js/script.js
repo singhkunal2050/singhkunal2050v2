9cb8b9b063f0fa0543567d81cd06007ab07de708
singhkunal2050 <singhkunal2050@gmail.com>
2021-07-05 19:30:06 +0530

    final

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,7 @@ const showGreeting = () =>{
   document.querySelector('.greeting').classList.toggle('show-greeting')
 }
 
-setTimeout( showGreeting , 10);
+setTimeout( showGreeting , 10000);
 
 document.querySelector('.close').addEventListener('click' , ()=>{
   document.querySelector('.greeting').classList.toggle('show-greeting')
