f60a6bc286e3fc6837c5e4effc41547f9dd9d557
singhkunal2050 <singhkunal2050@gmail.com>
2021-07-08 19:58:15 +0530

    added scroll skew with js

--- src/js/script.js
+++ src/js/script.js
@@ -7,3 +7,25 @@ setTimeout( showGreeting , 10000);
 document.querySelector('.close').addEventListener('click' , ()=>{
   document.querySelector('.greeting').classList.toggle('show-greeting')
 })
+
+// skew 
+
+const section = document.querySelectorAll('section');
+
+let currentPos = window.pageYOffset;
+
+const update = () => {
+	const newPos = window.pageYOffset;
+	const diff = newPos - currentPos;
+	const speed = diff * 0.35;
+	
+	section.forEach((elem) => {
+    elem.style.transform = `skewY(${ speed % 20 }deg)`
+  });
+	
+	currentPos = newPos;
+	
+	requestAnimationFrame(update);
+}
+
+update();
