799287ffd56f714b60c68a5c916558395ed3f103
singhkunal2050 <singhkunal2050@gmail.com>
2021-07-08 19:48:29 +0530

    update

--- src/js/script.js
+++ src/js/script.js
@@ -7,23 +7,3 @@ setTimeout( showGreeting , 10000);
 document.querySelector('.close').addEventListener('click' , ()=>{
   document.querySelector('.greeting').classList.toggle('show-greeting')
 })
-
-// gsap skew on scroll 
-
-let proxy = { skew: 0 },
-    skewSetter = gsap.quickSetter("section", "skewY", "deg"), // fast
-    clamp = gsap.utils.clamp(-20, 20); // don't let the skew go beyond 20 degrees. 
-
-ScrollTrigger.create({
-  onUpdate: (self) => {
-    let skew = clamp(self.getVelocity() / -300);
-    // only do something if the skew is MORE severe. Remember, we're always tweening back to 0, so if the user slows their scrolling quickly, it's more natural to just let the tween handle that smoothly rather than jumping to the smaller skew.
-    if (Math.abs(skew) > Math.abs(proxy.skew)) {
-      proxy.skew = skew;
-      gsap.to(proxy, {skew: 0, duration: 0.4, ease: "power1", overwrite: true, onUpdate: () => skewSetter(proxy.skew)});
-    }
-  }
-});
-
-// make the right edge "stick" to the scroll bar. force3D: true improves performance
-gsap.set(".skewElem", {transformOrigin: "right center", force3D: true});
