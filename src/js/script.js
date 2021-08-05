a408805663f3af81db1d834b2ba0e84a25a1c18b
singhkunal2050 <singhkunal2050@gmail.com>
2021-08-05 21:02:22 +0530

    added cookie after new blog popup close for 1day

--- src/js/script.js
+++ src/js/script.js
@@ -24,15 +24,43 @@ document.querySelector('.close').addEventListener('click' , ()=>{
 
 
 // show latest blog 
+
+function setCookie(name,value,days) {
+  var expires = "";
+  if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days*24*60*60*1000));
+      expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0;i < ca.length;i++) {
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+  }
+  return null;
+}
+
+function eraseCookie(name) {   
+  document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
 const showLatestBlog = () =>{
-  if(document.URL.split('/').length == 4 ) { // if at home 
+  if(document.URL.split('/').length == 4  && !getCookie('latest-blog-shown')) { // if at home 
     document.querySelector('.latest-blog-strip-container').classList.toggle('show-blog-strip')
   }
 }
-setTimeout( showLatestBlog , 2000);
+setTimeout( showLatestBlog , 4000);
+
 document.querySelector('.close-blog-strip').addEventListener('click' , ()=>{
   document.querySelector('.latest-blog-strip-container').classList.toggle('show-blog-strip')
+  setCookie('latest-blog-shown',true,1);
 })
+
 
 
 // skew on scroll
